0eb87c7a86a4cbf1473820b96d1b0c737af5de4d
Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
2022-11-18 16:21:01 -0800

    NewConnector unit test
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>

--- connector_test.go
+++ connector_test.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"github.com/databricks/databricks-sql-go/internal/config"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
+	"time"
 )
 
 func TestConnector_Connect(t *testing.T) {
@@ -22,44 +24,46 @@ func TestConnector_Connect(t *testing.T) {
 }
 
 func TestNewConnector(t *testing.T) {
-	//t.Run("Connector initialized with functional options should have all options set", func(t *testing.T) {
-	//	host := "databricks-host"
-	//	port := 1
-	//	accessToken := "token"
-	//	httpPath := "http-path"
-	//	maxRows := 100
-	//	timeout := 100
-	//	catalog := "catalog-name"
-	//	schema := "schema-string"
-	//	userAgentEntry := "user-agent"
-	//	sessionParams := map[string]string{"key": "value"}
-	//	connector, err := NewConnector(
-	//		WithServerHostname(host),
-	//		WithPort(port),
-	//		WithAccessToken(accessToken),
-	//		WithHTTPPath(httpPath),
-	//		WithMaxRows(maxRows),
-	//		WithTimeout(timeout),
-	//		WithInitialNamespace(catalog, schema),
-	//		WithUserAgentEntry(userAgentEntry),
-	//		WithSessionParams(sessionParams),
-	//	)
-	//	expectedUserConfig := config.UserConfig{
-	//		Host:                host,
-	//		Port:                port,
-	//		AccessToken:         accessToken,
-	//		HTTPPath:            httpPath,
-	//		MaxRows:             maxRows,
-	//		QueryTimeoutSeconds: timeout,
-	//		Catalog:             catalog,
-	//		Schema:              schema,
-	//		UserAgentEntry:      userAgentEntry,
-	//		SessionParams:       sessionParams,
-	//	}
-	//	expectedCfg := config.WithDefaults()
-	//	expectedCfg.UserConfig = expectedUserConfig
-	//	resultCfg := connector.cfg // ??? something like this possible ??? this doesn't compile
-	//	assert.Nil(t, err)
-	//	assert.Equal(t, expectedCfg, resultCfg)
-	//})
+	t.Run("Connector initialized with functional options should have all options set", func(t *testing.T) {
+		host := "databricks-host"
+		port := 1
+		accessToken := "token"
+		httpPath := "http-path"
+		maxRows := 100
+		timeout := 100 * time.Second
+		catalog := "catalog-name"
+		schema := "schema-string"
+		userAgentEntry := "user-agent"
+		sessionParams := map[string]string{"key": "value"}
+		con, err := NewConnector(
+			WithServerHostname(host),
+			WithPort(port),
+			WithAccessToken(accessToken),
+			WithHTTPPath(httpPath),
+			WithMaxRows(maxRows),
+			WithTimeout(timeout),
+			WithInitialNamespace(catalog, schema),
+			WithUserAgentEntry(userAgentEntry),
+			WithSessionParams(sessionParams),
+		)
+		expectedUserConfig := config.UserConfig{
+			Host:           host,
+			Port:           port,
+			Protocol:       "https",
+			AccessToken:    accessToken,
+			HTTPPath:       httpPath,
+			MaxRows:        maxRows,
+			QueryTimeout:   timeout,
+			Catalog:        catalog,
+			Schema:         schema,
+			UserAgentEntry: userAgentEntry,
+			SessionParams:  sessionParams,
+		}
+		expectedCfg := config.WithDefaults()
+		expectedCfg.UserConfig = expectedUserConfig
+		coni, ok := con.(*connector)
+		require.True(t, ok)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedCfg, coni.cfg)
+	})
 }
