a714c1114ec5635aa4612dc4e105e98ecd0f86dd
Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
2023-07-24 17:25:04 -0700

    Initial test for cloudURL Fetch in batchloader.go
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>

---
+++ internal/rows/arrowbased/batchloader_test.go
@@ -0,0 +1,89 @@
+package arrowbased
+
+import (
+	"bytes"
+	"context"
+	"github.com/apache/arrow/go/v11/arrow"
+	"github.com/apache/arrow/go/v11/arrow/array"
+	"github.com/apache/arrow/go/v11/arrow/ipc"
+	"github.com/apache/arrow/go/v11/arrow/memory"
+	"github.com/databricks/databricks-sql-go/internal/cli_service"
+	"github.com/databricks/databricks-sql-go/internal/config"
+	"github.com/pkg/errors"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
+)
+
+func generateMockArrowBytes() []byte {
+	mem := memory.NewCheckedAllocator(memory.NewGoAllocator())
+	defer mem.AssertSize(nil, 0)
+
+	fields := []arrow.Field{
+		{Name: "id", Type: arrow.PrimitiveTypes.Int32},
+		{Name: "name", Type: arrow.BinaryTypes.String},
+	}
+	schema := arrow.NewSchema(fields, nil)
+
+	builder := array.NewRecordBuilder(mem, schema)
+	defer builder.Release()
+
+	builder.Field(0).(*array.Int32Builder).AppendValues([]int32{1, 2, 3}, nil)
+	builder.Field(1).(*array.StringBuilder).AppendValues([]string{"one", "two", "three"}, nil)
+
+	record := builder.NewRecord()
+
+	var buf bytes.Buffer
+	w := ipc.NewWriter(&buf, ipc.WithSchema(record.Schema()))
+	if err := w.Write(record); err != nil {
+		return nil
+	}
+	if err := w.Close(); err != nil {
+		return nil
+	}
+	return buf.Bytes()
+}
+
+func TestBatchLoader(t *testing.T) {
+	testTable := []struct {
+		name             string
+		server           *httptest.Server
+		expectedResponse []*sparkArrowBatch
+		expectedErr      error
+	}{
+		{
+			name: "cloud-fetch-happy-case",
+			server: httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				w.WriteHeader(http.StatusOK)
+				w.Write(generateMockArrowBytes())
+			})),
+			expectedResponse: nil, // replace with []*sparkArrowBatch{}
+			expectedErr:      nil,
+		},
+	}
+
+	for _, tc := range testTable {
+		t.Run(tc.name, func(t *testing.T) {
+			defer tc.server.Close()
+
+			cu := &cloudURL{
+				TSparkArrowResultLink: &cli_service.TSparkArrowResultLink{
+					FileLink: tc.server.URL,
+				},
+			}
+
+			ctx := context.Background()
+			cfg := config.WithDefaults()
+
+			resp, err := cu.Fetch(ctx, cfg)
+
+			if !reflect.DeepEqual(resp, tc.expectedResponse) {
+				t.Errorf("expected (%v), got (%v)", tc.expectedResponse, resp)
+			}
+			if !errors.Is(err, tc.expectedErr) {
+				t.Errorf("expected (%v), got (%v)", tc.expectedErr, err)
+			}
+		})
+	}
+}
