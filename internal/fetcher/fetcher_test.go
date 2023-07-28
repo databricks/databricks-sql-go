33fe9b15ee221575a4218b374f7753519612a7ad
Matthew Kim <11141331+mattdeekay@users.noreply.github.com>
2023-07-28 15:22:44 -0700

    Add wait time and check expected fetch time for concurrent fetcher test
    Signed-off-by: Matthew Kim <11141331+mattdeekay@users.noreply.github.com>

--- internal/fetcher/fetcher_test.go
+++ internal/fetcher/fetcher_test.go
@@ -3,12 +3,15 @@ package fetcher
 import (
 	"context"
 	"github.com/databricks/databricks-sql-go/internal/config"
+	"math"
 	"testing"
+	"time"
 )
 
 // Create a mock struct for FetchableItems
 type mockFetchableItem struct {
 	item int
+	wait time.Duration
 	err  error
 }
 
@@ -18,6 +21,7 @@ type mockOutput struct {
 
 // Implement the Fetch method
 func (m *mockFetchableItem) Fetch(ctx context.Context, cfg *config.Config) ([]*mockOutput, error) {
+	time.Sleep(m.wait)
 	outputs := make([]*mockOutput, 5)
 	for i := range outputs {
 		sampleOutput := mockOutput{item: m.item}
@@ -34,7 +38,7 @@ func TestConcurrentFetcher(t *testing.T) {
 		cfg := &config.Config{}
 		inputChan := make(chan FetchableItems[*mockOutput], 10)
 		for i := 0; i < 10; i++ {
-			item := mockFetchableItem{item: i}
+			item := mockFetchableItem{item: i, wait: 1 * time.Second}
 			inputChan <- &item
 		}
 		close(inputChan)
@@ -45,6 +49,7 @@ func TestConcurrentFetcher(t *testing.T) {
 			t.Fatalf("Error creating fetcher: %v", err)
 		}
 
+		start := time.Now()
 		outChan, _, err := fetcher.Start()
 		if err != nil {
 			t.Fatalf("Error starting fetcher: %v", err)
@@ -64,6 +69,15 @@ func TestConcurrentFetcher(t *testing.T) {
 		// Check if the fetcher returned an error
 		if fetcher.Err() != nil {
 			t.Errorf("Fetcher returned an error: %v", fetcher.Err())
+		}
+
+		// Check if the fetcher took around the estimated amount of time
+		timeElapsed := time.Since(start)
+		rounds := int(math.Ceil(float64(10) / 3))
+		expectedTime := time.Duration(rounds) * time.Second
+		buffer := 100 * time.Millisecond
+		if timeElapsed-expectedTime > buffer {
+			t.Errorf("Expected fetcher to take around %d ms, took %d ms", int64(expectedTime/time.Millisecond), int64(timeElapsed/time.Millisecond))
 		}
 	})
 }
